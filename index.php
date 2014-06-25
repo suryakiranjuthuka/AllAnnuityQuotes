41cf485621ffdecd32ea258086144695e2dff9f5
Surya Kiran Juthuka <suryakiran.kittu@gmail.com>
2014-06-25 15:37:32 -0400

    initial

---
+++ index.php
@@ -0,0 +1,258 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<!--<meta name="google" content="notranslate">-->
+<meta http-equiv="Content-Language" content="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<title>All Annuity Quotes</title>
+<!--<link rel="shortcut icon" href="../site_images/plum_logo-favicon.ico" >-->
+<link href="stylesheets/annuity_leads.css" rel="stylesheet" type="text/css" media="screen,projection">
+<link rel="stylesheet" type="text/css" href="../stylesheets/component.css" />
+</head>
+
+<body>
+
+
+<a id="TOP"></a>
+
+<header> <!--class="bottomShadow"-->
+	<div id="lineSeperator"><h1 id="title">All <span>Annuity Quotes</span></h1></div>
+    
+    <nav id="mainButtonsBackgroundContainer">
+
+	<div id="smallTagLine">Don't Outline Your Savings</div>
+    <div id="bigTagLine">RELY ON AN ANNUITY</div>
+    <div id="mainButtonsContainer">
+            <div id="calculatorButton">Calculate My Payouts</div>
+            <div id="formButton">My Free Quote Request</div>
+    </div>
+	</nav>
+</header>
+
+
+<div id="container"><!-- ***** START CONTAINER***** -->
+
+
+<!--*************************************************************** Start Of Annuity Types ******************************************************-->
+<section id="typesOfAnnuity">
+	<h1>Variety Of Different Annuity Types</h1>
+    
+    <div id="circleTypesContainer">
+    	<div id="fixedType"><p>FIXED</p></div>
+        <div id="variableType"><p>VARIABLE</p></div>
+        <div id="immediateType"><p>IMMEDIATE</p></div>
+        <div id="indexedType"><p>INDEXED</p></div>
+        <div id="defferedType"><p>DEFFERED</p></div>
+    </div>
+    
+    <div id="circleTypesInfoContainer">
+    	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    
+    <div id="typesOfAnnuityDescription">
+    	Each of these annuities are designed and optimized for different financial portfolios, we can help you find which is best for you!
+    </div>
+      
+</section>
+<!--**************************************************************** END OF Annuity Types ******************************************************-->
+
+
+
+
+
+
+
+
+<!--*************************************************************** Start FORM ******************************************************-->
+<section id="annuityFormContainer">
+
+	<h1>My Free Quote Request</h1>
+    
+    <div id="formInstructions">
+    	Fill out the form below to be contacted by a Financial Advisor from your area.
+    </div>
+
+		<div id="form" class="md-content"></br>
+      		<h2>No-Obligation, Just Rates</h2>
+				<form action="index.php" method="post">
+                  <p><label>First Name*</label><input required name="first_name" type="text" /></p>
+                  <p><label>Last Name*</label><input required name="last_name" type="text" /></p>
+                  <p><label>Email*</label><input required name="email" type="text" /></p>
+                  <p><label>Phone Number*</label><input required name="phone_number" type="text" /></p>
+                  <p><label>Zip Code*</label><input required name="zip_code" type="text" /></p>
+                  <p><label>state*</label>
+                  <select required name="state">
+                        <option value="">----------</option>
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District Of Columbia</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin<option>
+                        <option value="WY">Wyoming</option>
+                   </select></p>
+                  <p><label>Investment Amount*</label>
+                   <select name="investment_amount" required>
+                        <option value="">---------</option>
+                        <option value="25K">Under $25,000</option>
+                        <option value="50K">$25,000 - $50,000</option>
+                        <option value="75K">$50,000 - $75,000</option>
+                        <option value="100K">$75,000 - $100,000</option>
+                        <option value="150K">$100,000 - $150,000</option>
+                        <option value="200K">$150,000 - $200,000</option>
+                        <option value="250K">$200,000 - $250,000</option>
+                        <option value="300K">$250,000 - $300,000</option>
+                        <option value="400K">$300,000 - $400,000</option>
+                        <option value="500K">$400,000 - $500,000</option>
+                        <option value="600K">$500,000 - $600,000</option>
+                        <option value="700K">$600,000 - $700,000</option>
+                        <option value="800K">$700,000 - $800,000</option>
+                        <option value="900K">$800,000 - $900,000</option>
+                        <option value="1M">$900,000 - $1,000,000</option>
+                        <option value="1M+">$1,000,000 or More</option>
+                    </select>
+                  </p>
+                  <div class="loader" id="loader_c_e"></div>
+                  <button id="submit" name="submit" type="submit">Submit</button></br>
+                </form>
+      	</div>
+        
+        
+     <div id="formEndingText">
+    	Fill out the form below to be contacted by a Financial Advisor from your area.
+    </div>
+      
+</section>
+<!--**************************************************************** END OF FORM ******************************************************-->
+
+
+
+
+
+
+
+
+<!--*************************************************************** Start Calculator ******************************************************-->
+<section id="annuityForm">
+	<h1>Calculate My Payouts</h1>
+      
+</section>
+<!--**************************************************************** END OF Calculator ******************************************************-->
+
+
+
+
+
+
+
+
+<!--*************************************************************** Start Sponcers ******************************************************-->
+<section id="sponcers">
+
+      
+</section>
+<!--**************************************************************** END OF Sponcers ******************************************************-->
+
+
+
+
+
+<footer>
+	Copyright © Allannuityquotes.com
+Respond and learn how life insurance and annuities can be used in various planning strategies for retirement. The insurance professionals are not associated with, or endorsed by any government agency. This information is for educational purposes only. Appointments may be made to discuss the applicability of information to your individual situation. This material has been prepared for informational and educational purposes only. It is not intended to provide, and should not be relied upon for, accounting, legal, tax or investment advice. Please consult with a professional specializing in these areas regarding the applicability of this information to your situation.
+</footer>
+
+
+
+
+
+    																<!--Start CLIENT EMAIL MODAL CONTENT -->  
+      
+<!--      <div class="md-modal md-effect-1" id="c_e_modal">
+		<div class="md-content"></br>
+      		<h2>Template</h2>
+				<form action="index.php" enctype="multipart/form-data" method="post">
+                  <p><label>Client Name*</label><input required name="c_e_client_name" type="text" /></p>
+                  <p><label>Sales Rep*</label><select required name="c_e_sales_rep">
+                  <option value=""></option>
+                  </select></p>
+                  <p><label>Send Date*</label><input required name="c_e_send_date" type="text" /></p>
+                  <p><label>Email List*</label><input required name="c_e_email_list" type="text" /></p>
+                  <p><label>Notes</label><textarea name="c_e_notes"  cols="40" rows="4" ></textarea></p>
+                  <input type="hidden" name="MAX_FILE_SIZE" value="5000000" />
+                  <p><label>Attachment</label><input type="file" name="c_e_attachment_url" /><p>
+                  <input type="hidden" id="c_e_website_url" type="text" name="c_e_website_url" value="" />
+                  <input class="md-close" type="hidden" id="c_e_url_path" type="text" name="c_e_url_path" value="" />
+                  <div class="loader" id="loader_c_e"></div>
+                  <button id="c_e_submit" name="c_e_submit" type="submit">Submit</button></br>
+                </form>
+      	</div>
+      </div>--><!--END CLIENT EMAIL MODAL CONTENT --> 
+
+
+</div><!-- ***** END OF CONTAINER***** -->
+
+<script type="text/javascript" src="../javascripts/jquery-2.0.2.min.js"></script>
+<script type="text/javascript" src="../javascripts/d3.min.js"></script>
+
+<script type="text/javascript">
+$(function(){
+    /*-- Scroll to link --*/
+    $('.scroller-link').click(function(e){
+        e.preventDefault(); //Don't automatically jump to the link
+        id = $(this).attr('href').replace('#', ''); //Extract the id of the element to jump to
+        $('html,body').animate({scrollTop: $("#"+id).offset().top-$(this).closest('ul').height()},'normal');
+    });
+});
+
+
+</script>
+
+<script type="text/javascript" src="../javascripts/annuity_leads.js"></script>
+</body>
+</html>
